106186cc25d6da1ff86818b4218919e2f2b48544
anais <dereumetz.anais@gmaiL.com>
2025-01-28 14:00:25 +0000

    commantaire

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js' // Build app Vue.js via les module ES (le plus utilisé)
 
 const app = createApp({
     data() {
@@ -9,4 +9,4 @@ const app = createApp({
     }
 })
 
-app.mount('#app');
+app.mount('#app'); // Relie à la div avec id=app
