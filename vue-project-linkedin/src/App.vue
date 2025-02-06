f9ab9615b4d0a982f2471bc919a2261377574693
anais <dereumetz.anais@gmaiL.com>
2025-02-06 14:12:55 +0000

    mes modifs

--- vue-project-linkedin/src/App.vue
+++ vue-project-linkedin/src/App.vue
@@ -3,8 +3,8 @@
 <template>
 
     <nav id="header">
-        <router-link to="/">HOME</router-link>
-        <router-link to="/contact">CONTACT</router-link>
+        <router-link :to="{name : 'Home'}">HOME</router-link>
+        <router-link :to="{name : 'Contact'}">CONTACT</router-link>
     </nav>
 
     <router-view />
